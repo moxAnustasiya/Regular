027b86b5b746fa03eb347c302f982747b6607802
moxAnustasiya <nassstasiya@gmail.com>
2022-09-14 15:21:53 +0300

    Изменена структура класса

--- src/regular.java
+++ src/regular.java
@@ -4,22 +4,10 @@ import java.util.regex.Pattern;
 public class regular
 {
     private static String regexIP = "^(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\." + "((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){2}"  + "(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])$";
-    private static String IP = "";
-    public regular(String address)
-    {
-        IP = address;
-    }
-    public static boolean IsIP(String textIp)
+    public static boolean IsIP(String IP)
     {
         Pattern pattern = Pattern.compile(regexIP);
-        Matcher matcher = pattern.matcher(textIp);
+        Matcher matcher = pattern.matcher(IP);
         return matcher.matches();
     }
-    //public static void main(String[] args)
-    //{
-       // Scanner in = new Scanner(System.in);
-      //  String adress = in.nextLine();
-       // if(IsIP(adress)) System.out.print("Является IP адрессом");
-       // else System.out.print("Не является IP адрессом");
-   // }
 }
