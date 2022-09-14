626c2e6992cecb6ad25f253ff92e52a5d5fc40a6
moxAnustasiya <nassstasiya@gmail.com>
2022-09-14 15:22:10 +0300

    Добавлен первый тест

--- src/regularTest.java
+++ src/regularTest.java
@@ -1,12 +1,14 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class regularTest {
-
+public class regularTest
+{
+    public static regular IP = new regular();;
     @Test
-    public void isIP()
+    public void isIP1()
     {
-
+        Assert.assertEquals(true, IP.IsIP("192.168.1.1"));
     }
 }
